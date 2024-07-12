a81adb9ff6171cb01da3a2b7f696173626308b5e
Sardor Shukurov <sardor_shukurov@epam.com>
2024-07-12 16:00:00 +0500

    add generic repository implementation

---
+++ Gamestore/src/Gamestore.DAL/Repository/Repository.cs
@@ -0,0 +1,72 @@
+using System.Linq.Expressions;
+using Gamestore.DAL.Common;
+using Gamestore.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gamestore.DAL.Repository;
+
+public class Repository<T> : IRepository<T>
+    where T : class, IBaseEntity
+{
+    private readonly MainDbContext _dbContext;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(MainDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<T>();
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var entity = await _dbSet.FindAsync(id);
+        if (entity is not null)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+
+    public async Task UpdateAsync(Guid id, T entity)
+    {
+        var existingEntity = await _dbSet.FindAsync(id);
+        if (existingEntity is not null)
+        {
+            _dbContext.Entry(existingEntity).CurrentValues.SetValues(entity);
+            _dbSet.Update(existingEntity);
+        }
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<T?> GetOneAsync(Expression<Func<T, bool>> filter)
+    {
+        return await _dbSet.AsNoTracking()
+            .FirstOrDefaultAsync(filter);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetAllByFilterAsync(Expression<Func<T, bool>> filter)
+    {
+        return await _dbSet.AsNoTracking()
+            .Where(filter)
+            .ToListAsync();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}
